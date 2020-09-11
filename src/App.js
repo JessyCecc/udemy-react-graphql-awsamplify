fd7026b35be0a4bcf89b65712a51ffd79e848009
dev-jc <jean-christophe.ceccaldi@oxatis.com>
2020-09-11 11:23:50 +0200

    commit part 2

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <hr>
+              PART 2 of the tutorial ...
         </p>
         <a
           className="App-link"
