2a49ef0bde1167257b9c2c5a2cdce93cc272fbf2
dev-jc <jean-christophe.ceccaldi@oxatis.com>
2020-09-11 11:25:36 +0200

    part 2

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <hr>
+          <hr />
               PART 2 of the tutorial ...
         </p>
         <a
